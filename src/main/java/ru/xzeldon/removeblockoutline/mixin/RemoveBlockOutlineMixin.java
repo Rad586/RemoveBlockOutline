390f4d5278e82fb4f26357b43a9c7ffc229bf888
Rad <99306685+Rad586@users.noreply.github.com>
2026-02-07 15:12:33 +0000

    only sneaking

--- src/main/java/ru/xzeldon/removeblockoutline/mixin/RemoveBlockOutlineMixin.java
+++ src/main/java/ru/xzeldon/removeblockoutline/mixin/RemoveBlockOutlineMixin.java
@@ -5,23 +5,18 @@ import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import ru.xzeldon.removeblockoutline.RemoveBlockOutlineClient;
-import ru.xzeldon.removeblockoutline.config.RemoveBlockOutlineConfig;
 
 @Mixin(WorldRenderer.class)
 public class RemoveBlockOutlineMixin {
-	private static RemoveBlockOutlineConfig config;
-
 	@Inject(method = "drawBlockOutline", at = @At("HEAD"), cancellable = true)
 	private void onDrawBlockOutline(MatrixStack matrixStack, VertexConsumer vertexConsumer, Entity entity, double cameraX, double cameraY, double cameraZ, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-		config = RemoveBlockOutlineClient.CONFIG;
-
-		if (!config.isEnableBlockOutline()) {
+		if (!((LivingEntity) entity).isSneaking()) {
 			ci.cancel();
 		}
 	}
