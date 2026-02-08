2c5844becca785b05e3f335d72270662abb094aa
Rad <99306685+Rad586@users.noreply.github.com>
2026-02-08 19:34:34 +0800

    Update RemoveBlockOutlineMixin.java

--- src/main/java/ru/xzeldon/removeblockoutline/mixin/RemoveBlockOutlineMixin.java
+++ src/main/java/ru/xzeldon/removeblockoutline/mixin/RemoveBlockOutlineMixin.java
@@ -1,11 +1,11 @@
 package ru.xzeldon.removeblockoutline.mixin;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,10 +14,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
 public class RemoveBlockOutlineMixin {
-	@Inject(method = "drawBlockOutline", at = @At("HEAD"), cancellable = true)
-	private void onDrawBlockOutline(MatrixStack matrixStack, VertexConsumer vertexConsumer, Entity entity, double cameraX, double cameraY, double cameraZ, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
-		if (!((LocalPlayer) entity).isSneaking() && !LocalPlayer.isMiningBlock()) {
-			ci.cancel();
-		}
-	}
+    @Inject(method = "drawBlockOutline", at = @At("HEAD"), cancellable = true)
+    private void onDrawBlockOutline(MatrixStack matrixStack, VertexConsumer vertexConsumer, Entity entity, double cameraX, double cameraY, double cameraZ, BlockPos blockPos, BlockState blockState, CallbackInfo ci) {
+        if (!MinecraftClient.getInstance().options.attackKey.isPressed() && !entity.isSneaking()) {
+            ci.cancel();
+        }
+    }
 }
